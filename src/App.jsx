2a2b316b4de89f90e2444df7e6e1952dc841d411
Kota Kanazawa <kota0818@gmail.com>
2024-12-11 20:36:32 +0900

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,31 @@ import { useActionState } from "react"
 import { updateNameInDB } from "./apis/updateNameInDB"
 
 function App() {
-  const [name, actionFunction, isPending] = useActionState(
-    updateName, // 非同期関数
-    JSON.parse(localStorage.getItem("name")) || "Anonymous user" // 初期値
+  const [state, actionFunction, isPending] = useActionState(
+    updateName,
+    {
+      error: null,
+      name: JSON.parse(localStorage.getItem("name")) || "Anonymous user"
+    }
   )
 
-  async function updateName(formData) {
+  async function updateName(prevState, formData) {
     try {
       const newName = await updateNameInDB(formData.get("name"))
-      return newName
+      return { name: newName, error: null }
     } catch (error) {
-      console.error(error.message)
+      return { error, name: prevState.name }
     }
   }
 
   return (
     <>
-      <p>Current user: {name}</p>
+      <p>Current user: {state.name}</p>
       {isPending && <p>Updating name...</p>}
       <form action={actionFunction}>
         <input type="text" name="name" required />
         <button type="submit">Update</button>
+        {!isPending && state.error && <p style={{ color: "red" }}>{state.error.message}</p>}
       </form>
     </>
   )
